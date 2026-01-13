1d54ad0f06cc1e791cb182110c2d4de9515b1116
Omkar Balasaheb Deshmukh <167670795+OmkarDeshmukh09@users.noreply.github.com>
2026-01-14 01:25:10 +0530

    Create program196.java

---
+++ program196.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+class Digit
+{
+    public int SumDigits(int iNo)
+    {
+        int iSum = 0 , iDigit = 0;
+
+        while (iNo != 0 ) 
+        {
+            iDigit = iNo % 10 ;
+            iSum = iSum + iDigit ; 
+            iNo = iNo / 10 ;
+        }
+
+        return iSum ;
+    }
+}
+
+class program91
+{
+    public static void main(String A [] ) 
+    {
+        int iValue = 0 , iRet = 0 ; 
+                
+        Scanner sobj = new Scanner(System.in);
+        
+        System.out.println("Enter Number :");
+        iValue = sobj.nextInt();
+    
+        Digit dobj = new Digit();
+        iRet = dobj.SumDigits(iValue);
+
+        System.out.println("Sum of Diggits is : "+iRet);
+
+    // Important
+        
+    sobj =  null;
+    dobj =  null;
+
+    System.gc();
+    }    
+}
