fd36d572b12fa507ca65519d4084e53062ea45c4
Omkar Balasaheb Deshmukh <167670795+OmkarDeshmukh09@users.noreply.github.com>
2026-03-18 23:28:13 +0530

    Add files via upload

---
+++ program440.java
@@ -0,0 +1,75 @@
+class node
+{
+    public int data;
+    public node next;
+}
+
+class SinglyLL 
+{
+    public node first;
+    public int iCount;
+
+    public SinglyLL()       //if not written public defalut hoil
+    {
+        System.out.println("Object of SinglyLL get created");
+        
+        this.first=null;
+        this.iCount= 0 ;
+    }
+
+    public void InsertFirst(int no)
+    {}
+    
+    public void InsertLast(int no)
+    {}
+    
+    public void InsertAtPos(int no , int pos)
+    {}
+
+    
+    public void DeleteFirst()
+    {}
+
+    public void DeleteLast()
+    {}
+
+    public void DeleteAtPos(int pos)
+    {}
+
+    public void Display()
+    {}
+
+    public int Count()
+    {
+        return this.iCount ;
+    }
+
+
+}
+
+class program440
+{
+    public static void main(String A[] ) 
+    {
+        int iRet = 0 ;
+        SinglyLL obj = null;
+
+        obj = new SinglyLL(); 
+
+        obj.InsertFirst(51);
+        obj.InsertFirst(21);
+        obj.InsertFirst(11);
+        
+        obj.Display();
+
+        iRet = obj.Count();
+        System.out.println("The numbers of nodes are : "+iRet);
+
+        
+        //Free the memory is Important for memory deallocation
+        obj = null;
+        System.gc();
+    
+    }
+    
+}
